10f84493ce4960a1f193e237de272d891cd3cd18
m32 <grzegorz.makarewicz@gmail.com>
2022-10-03 22:41:22 +0200

    minimal set of exported functions

--- far3-cc.cmd
+++ far3-cc.cmd
@@ -5,7 +5,7 @@ cl ^
 -Ifar3-include ^
 -Iw:\binw\Python-3.9.13.amd64\include ^
 far3python.cpp ^
-far3python.def ^
+far3python-min.def ^
 w:\binw\Python-3.9.13.amd64\libs\python39.lib
 
 copy far3python.dll W:\binw\far3-amd64-profile\Plugins\x64\python
