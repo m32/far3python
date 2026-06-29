9ea1602db2451ce434749304d17dbc92b69d45d8
mak <mak@trisoft.com.pl>
2026-06-29 23:34:20 +0200

    python 3.14.6

--- far3/__init__.py
+++ far3/__init__.py
@@ -1,5 +1,10 @@
-import uuid
-from far3.far3cffi import ffi, ffic
-from .pluginmanager import PluginManager
+try:
+    import uuid
+    from far3.far3cffi import ffi, ffic
+    from .pluginmanager import PluginManager
 
-pluginmanager = PluginManager()
+    pluginmanager = PluginManager()
+except:
+    import traceback
+    with open('w:/far3.err.log') as fp:
+        traceback.print_exc(file=fp)
